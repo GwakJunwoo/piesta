383329ead16fb90306c7374117439b5e9c2bbfe5
SANGHUN <tastybroccoli@naver.com>
2023-03-26 21:20:27 +0900

    modified pipeline.py

--- piesta/pipeline/pipeline.py
+++ piesta/pipeline/pipeline.py
@@ -4,81 +4,28 @@ import pandas as pd
 from itertools import islice 
 from graphviz import Diagraph
 
-class Pipeline:
-    def __init__(self, columns: Optional[Dict[str, Term]] = None,
-                 screen: Optional[Filter] = None):
-        self.columns = columns or {}
-        self.screen = screen
-
-    def add(self, name: str, term: Term):
-        self.columns[name] = term
-
-    def remove(self, name: str):
-        del self.columns[name]
-
-    def compile(self):
-        execution_plan = defaultdict(set)
-
-        def add_edge(input_term, output_term):
-            execution_plan[input_term].add(output_term)
-
-        def add_term(term):
-            for input_term in term.inputs:
-                add_edge(input_term, term)
-            for output_term in term.outputs:
-                add_edge(term, output_term)
-
-        # Add graph edges for each column.
-        for term in self.columns.values():
-            term.traverse_postorder(add_term)
-
-        return execution_plan
-
-    def visualize(self):
-        from graphviz import Digraph
-
-        execution_plan = self.compile()
-
-        dot = Digraph()
-
-        # Add nodes for each term.
-        for term in execution_plan:
-            dot.node(term.name, label=term.name)
-
-        # Add edges for each dependency.
-        for term, dependents in execution_plan.items():
-            for dependent in dependents:
-                dot.edge(term.name, dependent.name)
-
-        return dot
-
 class Portfolio_Pipeline(Pipeline):
     '''
     pf_pipeline = Portfolio_Pipeline(
-        [('SAA', 'GBI', {rf_rate:'UST10YR',}),
-         ('TAA', 'BL', {rf_rate:'UST2YR',}),
-         ('AP', 'MVO', {rf_rate:'UST3MO',})])
+        [('SAA', 'GBI', {"rf_rate":'UST10YR',}),
+         ('TAA', 'BL', {"rf_rate":'UST2YR',}),
+         ('AP', 'MVO', {"rf_rate":'UST3MO',})])
     '''
-    def __init__(self, steps):
+
+    def __init__(self, steps:list):
         self.steps = steps
 
-    def _iter(self,):
-        for idx, name, cvx_opt, params in enumerate(islice(self.steps, 0, len(self.steps))):
-            yield idx, name, cvx_opt, params
-    
-    def plot_pipeline(self, display='diagram', ):
-        graph = Digraph()
-        for _, (name, _, _) in self._iter():
-            graph.node(f'{name}:{cvx_opt}')
-
-        for idx in range(len(self.steps)-1):
-            graph.edge(
-    
+    def plot_pipeline(self,):
+        pipeline = Diagraph(name='pipeline', filename='pipeline', format='png', directory='./')
+        pipeline.attr('node', {'shape':'box', "color":'black', "penwidth":'1'}, label='Portfolio Pipeline')
+        for i in range(0,2):
+            pipeline.edges(f'[{self.steps[i][0]}]{self.steps[i][1]}', f'[{self.steps[i+1][0]}]{self.steps[i+1][1]}', label = "Constraints")
+        pipeline.view()
 
     def set_universe(self, screen_rule):
         self.universe = Universe(screen_rule)
 
-    def run(self, start_date, end_date, rebalancing_rule = 'MS'):
+    def run_backtest(self, start_date, end_date, rebalancing_rule = 'MS'):
         self.backtest = Backtest(self.steps, self.universe)
         self.backtest.run(start_date, end_date, rebalancing_rule)
         return self.backtest.result
@@ -95,16 +42,23 @@ class Backtest:
         self.universe = universe
         self.steps = steps
         self.weights = {}
-        self.strategy = {}
+        self.pipeline = []
         for name, cvx_opt, params in self._iter():
-            self.strategy.append(Strategy(name, cvx_opt, params))
+            name = Strategy(cvx_opt, params); self.pipeline.append(name)
+    
+    @property
+    def result(self,):
+        return self._result
+    
+    def _load_data(self,):
+        pass
 
     def _iter(self,):
         for name, cvx_opt, params in islice(self.steps, 0, len(self.steps)):
             yield name, cvx_opt, params
         
     def run(self, start_date, end_date, rebalancing_rule):
-        for rebalancing_date in pd.date_range(start_date, end_date).resample(rebalancing_rule).first():
-            for strategy in self.strategy:
-                weights = strategy.get_optimal_weights(constraint=weights if strategy.name in ['TAA', 'AP'] else None)
+        for rebalancing_date in pd.date_range(start_date, end_date, freq=rebalancing_rule):
+            for strategy in self.pipeline:
+                weights = strategy.get_optimal_weights(params, constraint=weights if strategy.name in ['TAA', 'AP'] else None)
             self.weights[rebalancing_date] = weights
