b0a8527d385dc4a032270161f38941904df854e9
SANGHUN <tastybroccoli@naver.com>
2023-03-26 19:54:52 +0900

    modified pipeline.py

--- piesta/pipeline/engine.py
+++ piesta/pipeline/engine.py
@@ -1,74 +1,388 @@
 
-class Engine:
-    def __init__(self):
-        self.asset_data = {}
-        
-    def add_asset(self, asset_name, asset_data):
-        self.asset_data[asset_name] = asset_data
-        
-    def run(self, pipeline_class, start_date, end_date, constraints_manager=None):
-        pipeline = pipeline_class()
-        for name, term in pipeline.columns.items():
-            asset_data = self.asset_data[term.asset_name]
-            data = asset_data[term.column]
-            term.set_data(data)
-            pipeline.add_column(name, term)
-            
-        if constraints_manager is not None:
-            composite_constraint = constraints_manager.get_composite_constraint()
-            pipeline.set_screen(composite_constraint)
-            
-        return pipeline.run_pipeline(start_date, end_date)
+class Pipeline(_BaseComposition):
+    """
+    Pipeline of transforms with a final estimator.
+    Sequentially apply a list of transforms and a final estimator.
+    Intermediate steps of the pipeline must be 'transforms', that is, they
+    must implement `fit` and `transform` methods.
+    The final estimator only needs to implement `fit`.
+    The transformers in the pipeline can be cached using ``memory`` argument.
+    The purpose of the pipeline is to assemble several steps that can be
+    cross-validated together while setting different parameters. For this, it
+    enables setting parameters of the various steps using their names and the
+    parameter name separated by a `'__'`, as in the example below. A step's
+    estimator may be replaced entirely by setting the parameter with its name
+    to another estimator, or a transformer removed by setting it to
+    `'passthrough'` or `None`.
+    ----------
+    steps : list of tuple
+        List of (name, transform) tuples (implementing `fit`/`transform`) that
+        are chained in sequential order. The last transform must be an
+        estimator.
+    memory : str or object with the joblib.Memory interface, default=None
+        Used to cache the fitted transformers of the pipeline. By default,
+        no caching is performed. If a string is given, it is the path to
+        the caching directory. Enabling caching triggers a clone of
+        the transformers before fitting. Therefore, the transformer
+        instance given to the pipeline cannot be inspected
+        directly. Use the attribute ``named_steps`` or ``steps`` to
+        inspect estimators within the pipeline. Caching the
+        transformers is advantageous when fitting is time consuming.
+    verbose : bool, default=False
+        If True, the time elapsed while fitting each step will be printed as it
+        is completed.
+    Attributes
+    ----------
+    named_steps : :class:`~sklearn.utils.Bunch`
+        Dictionary-like object, with the following attributes.
+        Read-only attribute to access any step parameter by user given name.
+        Keys are step names and values are steps parameters.
+    classes_ : ndarray of shape (n_classes,)
+        The classes labels. Only exist if the last step of the pipeline is a
+        classifier.
+    n_features_in_ : int
+        Number of features seen during :term:`fit`. Only defined if the
+        underlying first estimator in `steps` exposes such an attribute
+        when fit.
+        .. versionadded:: 0.24
+    feature_names_in_ : ndarray of shape (`n_features_in_`,)
+        Names of features seen during :term:`fit`. Only defined if the
+        underlying estimator exposes such an attribute when fit.
+        .. versionadded:: 1.0
+    See Also
+    --------
+    make_pipeline : Convenience function for simplified pipeline construction.
+    Examples
+    --------
+    >>> from sklearn.svm import SVC
+    >>> from sklearn.preprocessing import StandardScaler
+    >>> from sklearn.datasets import make_classification
+    >>> from sklearn.model_selection import train_test_split
+    >>> from sklearn.pipeline import Pipeline
+    >>> X, y = make_classification(random_state=0)
+    >>> X_train, X_test, y_train, y_test = train_test_split(X, y,
+    ...                                                     random_state=0)
+    >>> pipe = Pipeline([('scaler', StandardScaler()), ('svc', SVC())])
+    >>> # The pipeline can be used as any other estimator
+    >>> # and avoids leaking the test set into the train set
+    >>> pipe.fit(X_train, y_train)
+    Pipeline(steps=[('scaler', StandardScaler()), ('svc', SVC())])
+    >>> pipe.score(X_test, y_test)
+    0.88
+    """
 
-class PipelineEngine:
-    """
-    Engine for computing pipeline results.
-    """
-    def __init__(self):
-        self._cache = {}
-        
-    def run_pipeline(self, pipeline, start_date, end_date):
+    # BaseEstimator interface
+    _required_parameters = ["steps"]
+
+    def __init__(self, steps, *, memory=None, verbose=False):
+        self.steps = steps
+        self.memory = memory
+        self.verbose = verbose
+
+    def set_output(self, *, transform=None):
+        """Set the output container when `"transform"` and `"fit_transform"` are called.
+        Calling `set_output` will set the output of all estimators in `steps`.
+        Parameters
+        ----------
+        transform : {"default", "pandas"}, default=None
+            Configure output of `transform` and `fit_transform`.
+            - `"default"`: Default output format of a transformer
+            - `"pandas"`: DataFrame output
+            - `None`: Transform configuration is unchanged
+        Returns
+        -------
+        self : estimator instance
+            Estimator instance.
         """
-        Compute pipeline results over a range of dates.
+        for _, _, step in self._iter():
+            _safe_set_output(step, transform=transform)
+        return self
+
+    def get_params(self, deep=True):
+        """Get parameters for this estimator.
+        Returns the parameters given in the constructor as well as the
+        estimators contained within the `steps` of the `Pipeline`.
+        Parameters
+        ----------
+        deep : bool, default=True
+            If True, will return the parameters for this estimator and
+            contained subobjects that are estimators.
+        Returns
+        -------
+        params : mapping of string to any
+            Parameter names mapped to their values.
         """
-        dates = pipeline._compute_dates(start_date, end_date)
-        results = {}
-        
-        for date in dates:
-            if date in self._cache:
-                # Use cached results if available.
-                results[date] = self._cache[date]
+        return self._get_params("steps", deep=deep)
+
+    def set_params(self, **kwargs):
+        """Set the parameters of this estimator.
+        Valid parameter keys can be listed with ``get_params()``. Note that
+        you can directly set the parameters of the estimators contained in
+        `steps`.
+        Parameters
+        ----------
+        **kwargs : dict
+            Parameters of this estimator or parameters of estimators contained
+            in `steps`. Parameters of the steps may be set using its name and
+            the parameter name separated by a '__'.
+        Returns
+        -------
+        self : object
+            Pipeline class instance.
+        """
+        self._set_params("steps", **kwargs)
+        return self
+
+    def _validate_steps(self):
+        names, estimators = zip(*self.steps)
+
+        # validate names
+        self._validate_names(names)
+
+        # validate estimators
+        transformers = estimators[:-1]
+        estimator = estimators[-1]
+
+        for t in transformers:
+            if t is None or t == "passthrough":
+                continue
+            if not (hasattr(t, "fit") or hasattr(t, "fit_transform")) or not hasattr(
+                t, "transform"
+            ):
+                raise TypeError(
+                    "All intermediate steps should be "
+                    "transformers and implement fit and transform "
+                    "or be the string 'passthrough' "
+                    "'%s' (type %s) doesn't" % (t, type(t))
+                )
+
+        # We allow last estimator to be None as an identity transformation
+        if (
+            estimator is not None
+            and estimator != "passthrough"
+            and not hasattr(estimator, "fit")
+        ):
+            raise TypeError(
+                "Last step of Pipeline should implement fit "
+                "or be the string 'passthrough'. "
+                "'%s' (type %s) doesn't" % (estimator, type(estimator))
+            )
+
+    def __getitem__(self, ind):
+        """Returns a sub-pipeline or a single estimator in the pipeline
+        Indexing with an integer will return an estimator; using a slice
+        returns another Pipeline instance which copies a slice of this
+        Pipeline. This copy is shallow: modifying (or fitting) estimators in
+        the sub-pipeline will affect the larger pipeline and vice-versa.
+        However, replacing a value in `step` will not affect a copy.
+        """
+        if isinstance(ind, slice):
+            if ind.step not in (1, None):
+                raise ValueError("Pipeline slicing only supports a step of 1")
+            return self.__class__(
+                self.steps[ind], memory=self.memory, verbose=self.verbose
+            )
+        try:
+            name, est = self.steps[ind]
+        except TypeError:
+            # Not an int, try get step by name
+            return self.named_steps[ind]
+        return est
+
+    @property
+    def _estimator_type(self):
+        return self.steps[-1][1]._estimator_type
+
+    @property
+    def named_steps(self):
+        """Access the steps by name.
+        Read-only attribute to access any step by given name.
+        Keys are steps names and values are the steps objects."""
+        # Use Bunch object to improve autocomplete
+        return Bunch(**dict(self.steps))
+
+    @property
+    def _final_estimator(self):
+        estimator = self.steps[-1][1]
+        return "passthrough" if estimator is None else estimator
+
+    def _log_message(self, step_idx):
+        if not self.verbose:
+            return None
+        name, _ = self.steps[step_idx]
+
+        return "(step %d of %d) Processing %s" % (step_idx + 1, len(self.steps), name)
+
+    def _check_fit_params(self, **fit_params):
+        fit_params_steps = {name: {} for name, step in self.steps if step is not None}
+        for pname, pval in fit_params.items():
+            if "__" not in pname:
+                raise ValueError(
+                    "Pipeline.fit does not accept the {} parameter. "
+                    "You can pass parameters to specific steps of your "
+                    "pipeline using the stepname__parameter format, e.g. "
+                    "`Pipeline.fit(X, y, logisticregression__sample_weight"
+                    "=sample_weight)`.".format(pname)
+                )
+            step, param = pname.split("__", 1)
+            fit_params_steps[step][param] = pval
+        return fit_params_steps
+
+    # Estimator interface
+
+    def _fit(self, X, y=None, **fit_params_steps):
+        # shallow copy of steps - this should really be steps_
+        self.steps = list(self.steps)
+        self._validate_steps()
+
+        fit_transform_one_cached = memory.cache(_fit_transform_one)
+
+        for step_idx, name, transformer in self._iter(
+            with_final=False, filter_passthrough=False
+        ):
+            if transformer is None or transformer == "passthrough":
+                with _print_elapsed_time("Pipeline", self._log_message(step_idx)):
+                    continue
+
+            if hasattr(memory, "location") and memory.location is None:
+                # we do not clone when caching is disabled to
+                # preserve backward compatibility
+                cloned_transformer = transformer
             else:
-                # Compute pipeline results for the date.
-                columns = pipeline.columns
-                graph = pipeline.graph
-                dependencies = pipeline.dependencies
-                outputs = {}
-                inputs = {}
+                cloned_transformer = clone(transformer)
+            # Fit or load from cache the current transformer
+            X, fitted_transformer = fit_transform_one_cached(
+                cloned_transformer,
+                X,
+                y,
+                None,
+                message_clsname="Pipeline",
+                message=self._log_message(step_idx),
+                **fit_params_steps[name],
+            )
+            # Replace the transformer of the step with the fitted
+            # transformer. This is necessary when loading the transformer
+            # from the cache.
+            self.steps[step_idx] = (name, fitted_transformer)
+        return X
 
-                # Initialize input values.
-                for node in graph:
-                    inputs[node] = {}
+    def fit(self, X, y=None, **fit_params):
+        """Fit the model.
+        """
+        fit_params_steps = self._check_fit_params(**fit_params)
+        Xt = self._fit(X, y, **fit_params_steps)
+        with _print_elapsed_time("Pipeline", self._log_message(len(self.steps) - 1)):
+            if self._final_estimator != "passthrough":
+                fit_params_last_step = fit_params_steps[self.steps[-1][0]]
+                self._final_estimator.fit(Xt, y, **fit_params_last_step)
 
-                # Populate input values.
-                for node in graph:
-                    for dep in dependencies[node]:
-                        if dep in columns:
-                            inputs[node][dep] = results[date - 1][dep]
-                        else:
-                            inputs[node][dep] = pipeline.inputs[dep][date]
+        return self
 
-                # Compute output values.
-                for node in graph:
-                    outputs[node] = graph[node](*[inputs[node][dep] for dep in dependencies[node]])
 
-                # Filter results by the pipeline screen.
-                screen = pipeline.screen
-                mask = screen.mask(*[outputs[col] for col in screen.inputs])
-                masked_outputs = {col: outputs[col][mask] for col in outputs}
+    def _can_transform(self):
+        return self._final_estimator == "passthrough" or hasattr(
+            self._final_estimator, "transform"
+        )
 
-                # Save results to the cache.
-                results[date] = masked_outputs
-                self._cache[date] = masked_outputs
-        
-        return results
+
+    def _can_inverse_transform(self):
+        return all(hasattr(t, "inverse_transform") for _, _, t in self._iter())
+
+    @available_if(_can_inverse_transform)
+    def inverse_transform(self, Xt):
+        """Apply `inverse_transform` for each step in a reverse order.
+        All estimators in the pipeline must support `inverse_transform`.
+        Parameters
+        ----------
+        Xt : array-like of shape (n_samples, n_transformed_features)
+            Data samples, where ``n_samples`` is the number of samples and
+            ``n_features`` is the number of features. Must fulfill
+            input requirements of last step of pipeline's
+            ``inverse_transform`` method.
+        Returns
+        -------
+        Xt : ndarray of shape (n_samples, n_features)
+            Inverse transformed data, that is, data in the original feature
+            space.
+        """
+        reverse_iter = reversed(list(self._iter()))
+        for _, _, transform in reverse_iter:
+            Xt = transform.inverse_transform(Xt)
+        return Xt
+
+    @available_if(_final_estimator_has("score"))
+    def score(self, X, y=None, sample_weight=None):
+        """Transform the data, and apply `score` with the final estimator.
+        Call `transform` of each transformer in the pipeline. The transformed
+        data are finally passed to the final estimator that calls
+        `score` method. Only valid if the final estimator implements `score`.
+        Parameters
+        ----------
+        X : iterable
+            Data to predict on. Must fulfill input requirements of first step
+            of the pipeline.
+        y : iterable, default=None
+            Targets used for scoring. Must fulfill label requirements for all
+            steps of the pipeline.
+        sample_weight : array-like, default=None
+            If not None, this argument is passed as ``sample_weight`` keyword
+            argument to the ``score`` method of the final estimator.
+        Returns
+        -------
+        score : float
+            Result of calling `score` on the final estimator.
+        """
+        Xt = X
+        for _, name, transform in self._iter(with_final=False):
+            Xt = transform.transform(Xt)
+        score_params = {}
+        if sample_weight is not None:
+            score_params["sample_weight"] = sample_weight
+        return self.steps[-1][1].score(Xt, y, **score_params)
+
+
+    def _more_tags(self):
+        # check if first estimator expects pairwise input
+        return {"pairwise": _safe_tags(self.steps[0][1], "pairwise")}
+
+    def get_feature_names_out(self, input_features=None):
+        """Get output feature names for transformation.
+        Transform input features using the pipeline.
+        Parameters
+        ----------
+        input_features : array-like of str or None, default=None
+            Input features.
+        Returns
+        -------
+        feature_names_out : ndarray of str objects
+            Transformed feature names.
+        """
+        feature_names_out = input_features
+        for _, name, transform in self._iter():
+            if not hasattr(transform, "get_feature_names_out"):
+                raise AttributeError(
+                    "Estimator {} does not provide get_feature_names_out. "
+                    "Did you mean to call pipeline[:-1].get_feature_names_out"
+                    "()?".format(name)
+                )
+            feature_names_out = transform.get_feature_names_out(feature_names_out)
+        return feature_names_out
+
+    def _sk_visual_block_(self):
+        _, estimators = zip(*self.steps)
+
+        def _get_name(name, est):
+            if est is None or est == "passthrough":
+                return f"{name}: passthrough"
+            # Is an estimator
+            return f"{name}: {est.__class__.__name__}"
+
+        names = [_get_name(name, est) for name, est in self.steps]
+        name_details = [str(est) for est in estimators]
+        return _VisualBlock(
+            "serial",
+            estimators,
+            names=names,
+            name_details=name_details,
+            dash_wrapped=False,
+        )
